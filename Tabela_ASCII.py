d67e02a0f8d57054817fba86a91b8711a2f771d1
Johnny Kava <johnnylk2003@hotmail.com>
2025-02-25 17:38:44 -0300

    Colocando a lista dentro da função

--- Tabela_ASCII.py
+++ Tabela_ASCII.py
@@ -1,8 +1,7 @@
-letras_maiusculas = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 
+def EncontraASCII(nome):
+    maiusculas = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 
                      77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]
 
-
-def EncontraASCII(nome, maiusculas):
     nome = nome.upper()
     soma_ascii = 0
 
