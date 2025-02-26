01cdc5494b72890ffc63f5bab43568a7a7b8e9e9
Johnny Kava <johnnylk2003@hotmail.com>
2025-02-26 11:29:39 -0300

    Melhorando Interface

--- app/tabela-hash.py
+++ app/tabela-hash.py
@@ -4,17 +4,30 @@ tamanho_vetor = 10
 
 lista_telefonica = [None] * tamanho_vetor
 
-for i in range(4):
+escolha = 0
 
-    print("----------------- Cadastro -----------------")
-    nome = input("Digite o nome da pessoa que deseja colocar na lista: ")
-    telefone = input("Digite o telefone de " + nome + ": ")
+while escolha != '3':
+    print("---------------- MENU ----------------")
+    escolha = input("1- Adicionar Pessoa\n2- Consultar Número de Telefone da Pessoa\n3- Sair\n")
 
-    hash.setHash(nome, telefone, tamanho_vetor, lista_telefonica)
-    print("--------------------------------------------")
+    match escolha:
+        case '1':
+            print("----------------- Cadastro -----------------")
+            nome = input("Digite o nome da pessoa que deseja colocar na lista: ")
+            telefone = input("Digite o telefone de " + nome + ": ")
+            
+            hash.setHash(nome, telefone, tamanho_vetor, lista_telefonica)
+            print("--------------------------------------------")
 
-    print("----------------- Mostrando -----------------")
-    nome_busca = input("Digite o nome da pessoa que deseja colocar na lista: ")
+        case '2':
+            print("----------------- Mostrando -----------------")
+            nome_busca = input("Digite o nome da pessoa que deseja pesquisar na lista: ")
 
-    hash.getHash(nome_busca, tamanho_vetor, lista_telefonica)
-    print("--------------------------------------------")
+            hash.getHash(nome_busca, tamanho_vetor, lista_telefonica)
+            print("--------------------------------------------")
+
+        case '3':
+            print("Saindo!")
+
+        case _:
+            print("Opção invalida!")
